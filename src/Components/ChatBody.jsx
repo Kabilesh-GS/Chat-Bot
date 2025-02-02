c7708b42398054cbd0aed581a35310c99ec8ca34
Kabilesh-GS <kabileshgs@gmail.com>
2025-02-02 15:47:40 +0530

    Updated design

--- src/Components/ChatBody.jsx
+++ src/Components/ChatBody.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Chatsty from './ChatBody.module.css'
+import { FaArrowCircleUp } from "react-icons/fa"
 
 function ChatBody() {
 
@@ -27,19 +28,23 @@ function ChatBody() {
     <div className={Chatsty.frame}>
       <div className={Chatsty.body}>
         <div className="h-full rounded-xl overflow-y-auto">
-          { final &&
-            <p className="text-right bg-blue-500 mt-3 pr-4 p-1.5 mr-4 ml-120 rounded-lg">{final}</p>
+          {final &&
+            <p className="text-right text-white mt-3 pr-4 p-1.5 mr-4 ml-120 rounded-lg">{final}</p>
           }
-          <p className="text-left bg-blue-800 mr-100 ml-4 p-1.5 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nulla atque voluptatibus accusantium est repellat consequuntur repellendus aperiam dolorem. Iusto in ad autem temporibus a molestiae porro facilis libero mollitia.</p>
+          <p className="text-left text-white mr-100 ml-4 p-1.5 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nulla atque voluptatibus accusantium est repellat consequuntur repellendus aperiam dolorem. Iusto in ad autem temporibus a molestiae porro facilis libero mollitia.</p>
         </div>
-        <div className="flex flex-row  items-center">
+        <div style={{display: "flex",justifyContent: "center", alignItems: "center", flexDirection: "row"}}>
           <input 
           onKeyDown={handleKeyDown}
             value={ourMsg}
             onChange={(e)=>setourMsg(e.target.value)}
             className="bg-stone-700 mt-3 mb-4 border-2 border-white text-white text-lg rounded-xl block w-170 p-2.5 focus:ring-0 focus:ring-offset-0 outline-none" 
           />
-          <button type="button" className="bg-white hover:bg-gray-200 rounded-lg p-3" onClick={handleGenerate}>Generate</button>
+          <button 
+            type="button" 
+            className="flex justify-center bg-white hover:bg-gray-300 rounded-lg p-3 h-12 text-2xl w-15 ml-7" 
+            onClick={handleGenerate}>
+          <FaArrowCircleUp /></button>
         </div>
       </div>
     </div>
