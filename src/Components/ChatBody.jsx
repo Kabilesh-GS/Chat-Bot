abd336b10dd2ab84adccdd53a2008b7e7408aa4f
Kabilesh-GS <kabileshgs@gmail.com>
2025-04-14 12:44:58 +0530

    UI updates

--- src/Components/ChatBody.jsx
+++ src/Components/ChatBody.jsx
@@ -3,6 +3,7 @@ import Chatsty from './ChatBody.module.css'
 import { FaArrowCircleUp } from "react-icons/fa"
 import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
+import DefaultPrompt from './DefaultPrompt';
 
 function ChatBody() {
 
@@ -13,7 +14,7 @@ function ChatBody() {
   const handleGenerate = async () => {
     if (ourMsg.trim() === "") {
       const timeoutmsg = document.getElementById('errormsg');
-      timeoutmsg.innerHTML = "Please enter a message";
+      timeoutmsg.innerHTML = "Please enter a prompt";
       setTimeout(() => {timeoutmsg.innerHTML = ""},5000);
       return;
     } 
@@ -56,21 +57,26 @@ function ChatBody() {
   return (
     <div className={Chatsty.frame}>
       <div className={Chatsty.body}>
-        <div id='welcomeText' className="flex justify-center items-center mt-65">
+        <div id='welcomeText' className={Chatsty.Welcome}>
           <p className={Chatsty.welc}>Hello there!</p>
           <p className={Chatsty.welcQues}>How can I help you?</p>
+          <div className={Chatsty.defatutprom}>
+            <DefaultPrompt prompt="Write a code in java to sort an array with minimun time and space complexity" emoji="🧑‍💻"  onClick={()=>setourMsg("Write a code in java to sort an array with minimun time and space complexity")}/>
+            <DefaultPrompt prompt="Explain about the concept of big-bang theory and how universe was created" emoji="🤯" onClick={()=>setourMsg("Explain about the concept of big-bang theory and how universe was created")} />
+            <DefaultPrompt prompt="What is the difference between AI, ML, and Deep Learning?" emoji="🤖" onClick={()=>setourMsg("What is the difference between AI, ML, and Deep Learning?")} />
+          </div>
           &nbsp;
           <p className='text-red text-sm text-red-400' id='errormsg'></p>
         </div>
         <div className="h-full rounded-xl overflow-y-auto">
           {final && (
             <div className="flex justify-end">
-              <ReactMarkdown className="text-md text-emerald bg-white mt-3 pr-4 p-1.5 mr-4 ml-120 rounded-lg whitespace-pre-wrap break-words">{final}</ReactMarkdown>
+              <ReactMarkdown className="text-md max-w-160 text-end text-emerald bg-white mt-3 px-4 p-1.5 mr-4 ml-auto rounded-lg whitespace-pre-wrap break-words">{final}</ReactMarkdown>
             </div>
           )}
           {ResponseMsg && (
             <div className="flex justify-start">
-              <ReactMarkdown className="text-emerald text-lg border-l-4 border-indigo rounded-lg bg-sky break-words whitespace-pre-wrap mr-100 ml-4 p-1.5 mt-3">{ResponseMsg}</ReactMarkdown> 
+              <ReactMarkdown className="text-lg max-w-200 text-start text-emerald bg-sky px-3 border-l-4 border-indigo rounded-lg break-words whitespace-pre-wrap ml-4 p-1.5 mt-3">{ResponseMsg}</ReactMarkdown> 
             </div>
           )}
         </div>
@@ -80,11 +86,11 @@ function ChatBody() {
             value={ourMsg}
             placeholder='Ask anything'
             onChange={(e)=>setourMsg(e.target.value)}
-            className="bg-stone-700 mt-3 mb-4 resize-none border-emerald placeholder-stone text-emerald text-lg rounded-xl block w-170 h-13 p-2.5 focus:ring-none focus:ring-offset-0 bg-teal outline-none" 
+            className="bg-stone-700 mt-3 mb-4 pl-4 resize-none border-emerald placeholder-stone text-emerald text-lg rounded-full block w-170 h-13 p-2.5 focus:ring-none focus:ring-offset-0 bg-teal outline-none" 
           />
           <button 
             type="button" 
-            className="flex justify-center bg-cyan text-white hover:bg-cyan-50 rounded-lg p-3 h-12 text-2xl w-15 ml-7" 
+            className={Chatsty.sendbtn} 
             onClick={handleGenerate}>
           <FaArrowCircleUp /></button>
         </div>
