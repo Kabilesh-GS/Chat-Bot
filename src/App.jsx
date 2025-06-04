b86202f6033223d86b3498ce8fa58508d041928e
Kabilesh-GS <kabileshgs@gmail.com>
2025-06-04 18:03:44 +0530

    Secured API key and connected firestore to store users

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import ChatBody from './Components/ChatBody'
 import Header from './Components/Header'
-import {Signin,auth} from './Utility/Firebase/Firebase.utils';
+import {Signin,auth,createCollectionUsers} from './Utility/Firebase/Firebase.utils';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import SignIn from './Components/SignIn';
 
@@ -9,9 +9,8 @@ function App() {
   const[user] = useAuthState(auth);
   const signinFun = async () => {
     const res = await Signin();
-    console.log(res.user);
+    createCollectionUsers(res.user);
   }
-  console.log(user);
   return (
     <div>
       <Header />
--- src/Utility/Firebase/Firebase.utils.js
+++ src/Utility/Firebase/Firebase.utils.js
@@ -1,8 +1,9 @@
 import {initializeApp} from 'firebase/app'
+import { getFirestore,doc,getDoc,setDoc,collection,serverTimestamp } from 'firebase/firestore';
 import { getAuth,signInWithPopup,GoogleAuthProvider,signOut } from 'firebase/auth' 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0piRRe4KqE-1fsLl2PcnfqgSXK4Ie2GE",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "chat-bot-gemi.firebaseapp.com",
   projectId: "chat-bot-gemi",
   storageBucket: "chat-bot-gemi.firebasestorage.app",
@@ -16,3 +17,23 @@ export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const Signin = () => signInWithPopup(auth,provider);
 export const Signout = () => signOut(auth);
+
+export const db = getFirestore(app);
+export const createCollectionUsers = async (userAuth) => {
+  const UserDocument = doc(db, 'users', userAuth.uid);
+  const UserDocPresent = await getDoc(UserDocument);
+  if(!UserDocPresent.exists()){
+    const createdDate = new Date();
+    const { displayName,email } = userAuth;
+    try{
+      await setDoc(UserDocument, {
+        displayName,
+        email,
+        createdDate
+      })
+    }
+    catch(error){
+      console.log('An error occured',error)
+    }
+  }
+}
