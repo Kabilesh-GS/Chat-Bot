449e13f540f38fdc7f8fedcecf017704ed0867de
Kabilesh-GS <kabileshgs@gmail.com>
2025-06-06 16:59:02 +0530

    Added signin form

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -3,14 +3,25 @@ import GoogleIcon from '../assets/google.svg';
 function SignIn({signIn}) {
 
   return (
-    <div>
-      <p>SignIn with your account</p>
-        <div className='flex items-center justify-center'>
-        <button onClick={signIn} className='cursor-pointer w-[200px] active:scale-90 flex gap-2 justify-center p-3 border border-gray-300 transition-all duration-100 rounded-lg mt-4 hover:bg-blue-100 hover:rounded-[20px] transition' >
+    <div className='flex flex-col items-center'>
+      <div className='flex flex-col items-center mt-5'>
+        <p>SignIn with Google</p>
+        <button onClick={signIn} className='cursor-pointer w-[200px] active:scale-90 flex gap-2 justify-center px-3 py-2 border border-gray-300 transition-all duration-100 rounded-[15px] hover:scale-103 mt-2 hover:bg-blue-100 transition' >
           <img src={GoogleIcon} alt="Google" className='w-6 h-6' />
           <span>Google</span>
         </button>
       </div>
+      <div className='flex justify-center items-center mt-4'>
+        <div className='bg-black h-[2px] w-[110px] opacity-25 mr-3'></div><span>OR</span><div className='bg-black h-[2px] w-[110px] opacity-25 ml-3'></div>
+      </div>
+      <form onSubmit={() => {}} className='flex flex-col mt-5 w-70'>
+        <lable className="text-[18px]">E-mail</lable>
+        <input type='email' className="bg-white mb-2.5 p-1 rounded-lg" placeholder='E-mail'/>
+        <lable className="text-[18px]">Password</lable>
+        <input type='password' className="bg-white mb-2.5 p-1 rounded-lg" placeholder='Password'/>
+
+        <button className='cursor-pointer bg-black text-white p-3 rounded-xl' type='submit'>SignIn</button>
+      </form>
     </div>
   )
 }
--- src/Components/UserLogin.jsx
+++ src/Components/UserLogin.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './SignIn.module.css';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
@@ -7,6 +7,16 @@ import {useAuthState} from 'react-firebase-hooks/auth';
   
 function UserLogin() {
 
+  const SignUpHandle = () => {
+    document.getElementById('signUp').style.backgroundColor = '#3bb536';
+    document.getElementById('signIn').style.backgroundColor = '';
+  }
+
+  const SignInHandle = () => {
+    document.getElementById('signIn').style.backgroundColor = '#3bb536';
+    document.getElementById('signUp').style.backgroundColor = '';
+  }
+
   const[Active,setActive] = useState(true);
   const[user] = useAuthState(auth);
   const signinFun = async () => {
@@ -14,11 +24,19 @@ function UserLogin() {
     createCollectionUsers(res.user);
   }
 
+  useEffect(() => {
+  if (Active) {
+    SignUpHandle();
+  } else {
+    SignInHandle();
+  }
+}, [Active]);
+
   return (
     <div className='flex items-center justify-center flex-col mt-4'>
-      <div className='flex gap-30'>
-        <div><input checked={ Active} onChange={() => setActive(true) } type='radio' name='sign' className='cursor-pointer w-15 mt-2 opacity-0 absolute'/><span>Sign Up</span></div>
-        <div><input checked={!Active} onChange={() => setActive(false)} type='radio' name='sign' className='cursor-pointer w-15 mt-2 opacity-0 absolute'/><span>Sign In</span></div>
+      <div className='flex gap-20'>
+        <div><input checked={ Active} onClick={SignUpHandle} onChange={() => setActive(true) } type='radio' name='sign' className='cursor-pointer ml-5 w-15 mt-2 opacity-0 absolute'/><span id='signUp' className='rounded-lg py-1 px-5'>Sign Up</span></div>
+        <div><input checked={!Active} onClick={SignInHandle} onChange={() => setActive(false)} type='radio' name='sign' className='cursor-pointer ml-5 w-15 mt-2 opacity-0 absolute'/><span id='signIn' className='rounded-lg py-1 px-5'>Sign In</span></div>
       </div>
       <div>
         {Active ? <SignUp/> : <SignIn signIn={signinFun}/> }
